6aa41f8aaff7431f7b205be339f19afbe2a57197
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-06-13 14:53:33 +0300

    4 map pizzas , spread, active classes

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from 'react'
 
-export const Categories = ()=>{
+export const Categories = () => {
+		const [activeIndex, setActiveIndex] = React.useState(0)
+
+		const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+
 		return (
 				<div className="categories">
 						<ul>
-								<li className="active">Все</li>
-								<li>Мясные</li>
-								<li>Вегетарианская</li>
-								<li>Гриль</li>
-								<li>Острые</li>
-								<li>Закрытые</li>
+								{
+										categories.map((value, i) =>
+												<li onClick={() => setActiveIndex(i)}
+												    className={activeIndex === i ? 'active' : ''}>{value}
+												</li>)
+								}
 						</ul>
 				</div>
 		)
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
 
-export const PizzaBlock = ({title,price}) => {
-		const [pizzaCount, setPizzaCount] = React.useState(0)
+export const PizzaBlock = ({title, price, imageUrl, sizes, types}) => {
+		const typeNames = ['тонкое', 'традиционное']
 
-		const onClickAdd = () => {
-				setPizzaCount(pizzaCount+1)
-		}
+		const [activeType, setActiveType] = React.useState(0)
+		const [activeSize, setActiveSize] = React.useState(0)
 
 		return (
 				<div className="pizza-block">
 						<img
 								className="pizza-block__image"
-								src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+								src={imageUrl}
 								alt="Pizza"
 						/>
 						<h4 className="pizza-block__title">{title}</h4>
 						<div className="pizza-block__selector">
 								<ul>
-										<li className="active">тонкое</li>
-										<li>традиционное</li>
+										{
+												types.map(typeId =>
+														<li onClick={() => setActiveType(typeId)}
+														    className={activeType === typeId ? 'active' : ''}>
+																{typeNames[typeId]}
+														</li>)
+										}
 								</ul>
 								<ul>
-										<li className="active">26 см.
-										</li>
-										<li>30 см.</li>
-										<li>40 см.</li>
+										{
+												sizes.map((size, i) =>
+														<li className={activeSize === i ? 'active' : ''}
+														    onClick={() => setActiveSize(i)}>
+																{size} см.
+														</li>)
+										}
 								</ul>
 						</div>
 						<div className="pizza-block__bottom">
 								<div className="pizza-block__price">от {price} ₽</div>
-								<button onClick={onClickAdd} className="button button--outline button--add">
+								<button className="button button--outline button--add">
 										<svg
 												width="12"
 												height="12"
@@ -43,7 +50,7 @@ export const PizzaBlock = ({title,price}) => {
 												/>
 										</svg>
 										<span>Добавить</span>
-										<i>{pizzaCount}</i>
+										<i>0</i>
 								</button>
 						</div>
 				</div>
